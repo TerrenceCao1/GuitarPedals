05dc026a10efa16ae2c46556956651f908ca8367
TerrenceCao1 <terrencezcao@gmail.com>
2026-06-20 22:49:07 -0700

    added Boost + distortion + overdrive functions

--- inc/Effects.h
+++ inc/Effects.h
@@ -20,6 +20,61 @@
 
 namespace Effects 
 {
+	/*
+	 * @brief	Adds no effects, just copies the inFrame to the outFrame
+	 * @param	inFrame		input audio data frame
+	 * @param	outFrame	output audio data frame
+	 * @return	outputFrame gets filled
+	 */
 	void Bypass(const AudioFrame& inFrame, AudioFrame& outFrame);
+
+	/*
+	 * @brief	Increases volume without any other effects
+	 * @param	inFrame		input audio data frame
+	 * @param	outFrame	output audio data frame
+	 * @param	gainDb		how much volume increase
+	 * @return	outputFrame gets filled with boosted values
+	 */
+	void Boost(const AudioFrame& inFrame, AudioFrame& outFrame, double gainDb);
+
+	/*
+	 * @brief	Slices the output wave at the threshold amplitude
+	 * @param	inFrame		input audio data frame
+	 * @param	outFrame	output audio data frame
+	 * @param	gain		how much volume increase
+	 * @param	threshold	cutting off point for +-
+	 * @return	outputFrame gets hardClipped lol
+	 */
+	void HardClip(const AudioFrame& inFrame, AudioFrame& outFrame, double gain, double threshold = 0.5);
+
+	/*
+	 * @brief	Applies tanh function to smooth out waveform 
+	 * @param	inFrame		input audio data frame
+	 * @param	outFrame	output audio data frame
+	 * @param	gain		how much volume increase
+	 * @return	outputFrame gets softclipped
+	 */
+	void SoftClip(const AudioFrame& inFrame, AudioFrame& outFrame, double gain);
+
+	/*
+	 * @brief	Applies Distortion to audio
+	 * @param	inFrame		input audio data frame
+	 * @param	outFrame	output audio data frame
+	 * @param	gain		how much volume increase
+	 * @param	threshold	how hardclipped
+	 * @return	outputFrame gets distorted inFrame
+	 */
+	void Distort(const AudioFrame& inFrame, AudioFrame& outFrame, double gain, double threshold = 0.5);
+
+	/*
+	 * @brief	Applies Overdrive to audio
+	 * @param	inFrame		input audio data frame
+	 * @param	outFrame	output audio data frame
+	 * @param	gain		how much volume increase
+	 * @return	outputFrame gets overdrived inFrame
+	 */
+	void Overdrive(const AudioFrame& inFrame, AudioFrame& outFrame, double gain);
+
+
 }
 
