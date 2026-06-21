ef125ceeb00af784a15ae67cd26089da182134f5
TerrenceCao1 <terrencezcao@gmail.com>
2026-06-20 22:49:25 -0700

    boost effect

--- src/Effects.cpp
+++ src/Effects.cpp
@@ -6,3 +6,15 @@ void Effects::Bypass(const AudioFrame &inFrame, AudioFrame &outFrame)
 {
 	outFrame.getDataMutable() = inFrame.getData();
 }
+
+void Effects::Boost(const AudioFrame &inFrame, AudioFrame &outFrame, double gainDb)
+{
+	double linearGain = std::pow(10.0, gainDb / 20.0);
+	const auto& in = inFrame.getData();
+	auto& out = outFrame.getDataMutable();
+
+	for(size_t i = 0; i < in.size(); i++)
+	{
+		out[i] = in[i] * linearGain;
+	}
+}
